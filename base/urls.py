75413d791b819ec43dd6f7b7164a9817a61ca0df
idimitrisc47 <dimitrisc47@gmail.com>
2023-08-03 22:28:19 -0500

    Database & Admin Panel

--- base/urls.py
+++ base/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='Home'),
-    path('room/', views.room, name='Room'),
+    path('', views.home, name='home'),
+    path('room/<str:pk>', views.room, name='room'),
+    path('create-room/',views.createRoom,name='create-room'),
+    path('update-room/<str:pk>',views.updateRoom,name='update-room'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,redirect
+from .models import Room
+from .forms import RoomForm
 def home(request):
-    return render(request, 'home.html')
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html',context)
 
-def room(request):
-    return render(request, 'room.html')
+def room(request,pk):
+    room = Room.objects.get(id=pk)
+    context = {'room':room}
+    return render(request, 'base/room.html',context)
+
+def createRoom(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form':form}
+    return render(request,'base/room_form.html', context)
+
+def updateRoom(request,pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    context = {'form':form}
+    return render(request,'base/room_form.html',context)
